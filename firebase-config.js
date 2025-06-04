f716e81d0f615b509388b92cda15918a777c326c
Nurriyad98 <133578976+Nurriyad98@users.noreply.github.com>
2025-06-04 18:43:42 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCZXteKXZn-BLio2O1cTTeZqgCKXgHOYHQ",
+   authDomain: "liberty-ef330.firebaseapp.com",
+   databaseURL: "https://liberty-ef330-default-rtdb.firebaseio.com",
+  projectId: "liberty-ef330",
+    storageBucket: "liberty-ef330.appspot.com",
+    messagingSenderId: "829415722731",
+    appId: "1:829415722731:web:e9b300549e4c6ca61eb8ae"
+};
+
+firebase.initializeApp(firebaseConfig);
